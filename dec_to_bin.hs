0e9accfe9a3571bd88bf6dbcd6e0e23d374721ff
Filip Peterek <fpeterek@seznam.cz>
2017-09-17 14:24:49 +0200

    Update dec_to_bin.hs

--- dec_to_bin.hs
+++ dec_to_bin.hs
@@ -4,8 +4,7 @@ removeLastBit x = (x `shift` (-1)) `shift` 1
 
 lastBitToStr x = if x /= removeLastBit x then "1" else "0"
 
-decToBin :: Integer -> String
 decToBin x = if x /= 0 then (decToBin (x `shift` (-1))) ++ (lastBitToStr x) else "0"
 
 
-main = putStrLn (decToBin 52)
+main = putStrLn (decToBin (52::Integer))
